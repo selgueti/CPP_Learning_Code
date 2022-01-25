ec46d9d90dc563d3ab3ce7ec77c2cfa43ce791b2
selgueti <sofian.elguetibi@gmail.com>
2022-01-25 11:14:16 +0100

    A trainer can catch a pokemon

--- tp-03/Pokemons/Trainer.h
+++ tp-03/Pokemons/Trainer.h
@@ -2,25 +2,41 @@
 
 #include "PC.h"
 #include "Pokeball.h"
+#include "Pokemon.h"
 
+#include <iostream>
 #include <string>
 // A person that capture Pokemons and make them fight.
 class Trainer
 {
 public:
-    Trainer(const std::string& name, const PC& pc)
+    Trainer(const std::string& name, PC& pc)
         : _name { name }
         , _pc { pc }
     {
-        _pokeballs.resize(6);
+        _pokeballs.resize(_nb_pokebbals);
     }
 
     const std::string name() const { return _name; }
 
     const std::vector<Pokeball>& pokeballs() const { return _pokeballs; }
 
+    void capture(std::unique_ptr<Pokemon> pokemon)
+    {
+        for (auto i = 0u; i < _nb_pokebbals; i++)
+        {
+            if (_pokeballs[i].empty())
+            {
+                _pokeballs[i].store(std::move(pokemon));
+                return;
+            }
+        }
+        _pc.receive(std::move(pokemon));
+    }
+
 private:
     const std::string     _name;
-    const PC&             _pc;
+    PC&                   _pc;
     std::vector<Pokeball> _pokeballs;
+    const unsigned int    _nb_pokebbals = 6;
 };
