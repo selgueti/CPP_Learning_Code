6110a46c32830c151b7c5f2b3063a1abcfc7a1c7
selgueti <sofian.elguetibi@gmail.com>
2022-01-25 09:47:57 +0100

    PCs are constructed with 0 pokemon inside

--- tp-03/Pokemons/PC.h
+++ tp-03/Pokemons/PC.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#include "Pokemon.h"
+
+#include <memory>
+#include <vector>
 // A PC is the place where Pokemons get automagically sent when the trainer's pockets are full.
 // When a Pokemon is transferred to the PC, this one takes ownership.
 class PC
-{};
+{
+public:
+    std::vector<std::unique_ptr<Pokemon>>& pokemons() { return _pokemons; }
+
+private:
+    std::vector<std::unique_ptr<Pokemon>> _pokemons;
+};
