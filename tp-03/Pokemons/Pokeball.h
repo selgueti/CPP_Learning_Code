c209ff391f90c4d7f28960545f3b8d30c636ccb3
selgueti <sofian.elguetibi@gmail.com>
2022-01-25 09:40:06 +0100

    Pokemons can be stored in Pokeballs

--- tp-03/Pokemons/Pokeball.h
+++ tp-03/Pokemons/Pokeball.h
@@ -1,8 +1,17 @@
 #pragma once
 
+#include "Pokemon.h"
+
+#include <memory>
+#include <utility>
 // A ball where Pokemon sleep.
 class Pokeball
 {
 public:
-    bool empty() { return true; }
+    bool     empty() { return _pokemons.empty(); }
+    void     store(std::unique_ptr<Pokemon> pokemon) { _pokemons.emplace_back(std::move(pokemon)); }
+    Pokemon& pokemon() { return *(_pokemons[0]); }
+
+private:
+    std::vector<std::unique_ptr<Pokemon>> _pokemons {};
 };
